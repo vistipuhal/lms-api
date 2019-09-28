ad22ec9a605ffbd4c11e3dce70ffba6ff7c97db3
stephennimmo <stephen.nimmo@fairwaysoftware.com>
2019-09-28 09:28:25 -0500

    Jwt key string injection

--- src/main/java/com/fairwaysoftware/lms/security/TokenHelper.java
+++ src/main/java/com/fairwaysoftware/lms/security/TokenHelper.java
@@ -3,17 +3,28 @@ package com.fairwaysoftware.lms.security;
 import com.fairwaysoftware.lms.service.domain.User;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
 import java.security.Key;
 
 @Component
 public class TokenHelper {
 
     public static final String AUTHORIZATION_HEADER_PREFIX = "Bearer";
+    private Key key;
 
-    private Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    public TokenHelper(@Value("#{environment.JWT_KEY ?: 'thiskeyhastobeatleastthismanycharactershgahsd'}") String key) {
+        if (StringUtils.isEmpty(key) || key.length() < 44) {
+            throw new RuntimeException("JWT_KEY needs to be 44 characters long");
+        }
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+        byte[] keyBytes = DatatypeConverter.parseBase64Binary(key);
+        this.key = new SecretKeySpec(keyBytes, signatureAlgorithm.getJcaName());
+    }
 
     public String generateToken(User user) {
         return Jwts.builder().setSubject(user.getUserId().toString()).signWith(key).compact();
